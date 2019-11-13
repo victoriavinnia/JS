3d0846fd0f95840424b6b5dcc060923ed81b7a4c
victoriavinnia <victoriavinnia@gmail.com>
2019-11-13 22:40:46 +0300

    hw

---
+++ HW4/js/hw4.js
@@ -0,0 +1,37 @@
+//1. Допустим, пользователь вводит названия городов через пробел. Вы их присваиваете переменной.
+//Переставьте названия так, чтобы они были упорядочены по алфавиту.
+let cities = prompt("","");
+console.log(cities);
+let arr = cities.split(" ");
+console.log(arr.sort());
+
+//2. Найти все вхождения подстроки в строку. Строку и подстроку вводит пользователь.
+let sentence = prompt("Введите предложение", "Show must go on");
+let substring = prompt("Введите подстроку", "o");
+console.log(sentence);
+console.log(substring);
+
+for (let i = 0; i < sentence.length; i++) {
+    let str = sentence[i];
+    //console.log(str);
+    if(str === substring) {
+        console.log(i);
+    }
+};
+
+//3.Написать функцию, которая проверяет, является ли строка палиндромом (использовать стандартные методы)!
+// Палиндром — это слово или фраза, которые одинаково читаются по буквам или по словам как слева направо, так и справа налево.
+
+palindrome = str => {
+    str = str.toLowerCase();
+    console.log(str);
+    let tr = str.split('').reverse().join('');
+    console.log(tr);
+    if (str === tr) {
+        console.log("yes");
+    } else {
+        console.log("no");
+    }
+};
+
+palindrome("nna");
---
+++ HW5/js/hw5.js
@@ -0,0 +1,57 @@
+let goods = {
+    piano: {
+        title: "Пианино",
+        price: 3000,
+        count: 25
+    },
+    guitar: {
+        title: "Гитара",
+        price: 1200,
+        count: 40
+    },
+    drum: {
+        title: "Барабаны",
+        price: 2700,
+        count: 12
+    },
+    flute: {
+        title: "Флейта",
+        price: 900,
+        count: 50
+    },
+    harp: {
+        title: "Арфа",
+        price: 3400,
+        count: 5
+    }
+};
+
+//1.Написать функцию со следующими аргументами: объект, from (значение от), to (значение до).
+// Функция должна вернуть объект с товарами, цены которых лежат в диапазоне от значения from до значения to.
+// Пример вызова функции getGoods(goods, 2000, 3000); в данном случае функция должна вернуть все товары,
+// у которых цена в диапазоне от 2000 до 3000. Для проверки функции используйте объект! goods из файла forHw.js.
+function getGoods(obj, from, to) {
+    let arr = [];
+    for(let item in obj) {
+        if (obj[item].price >= from && obj[item].price <= to) {
+            arr.push(obj[item].title);
+        }
+    }
+    return arr;
+};
+
+let aprice = getGoods(goods, 2000, 3000);
+console.log(aprice);
+//2 задача
+
+
+/*
+
+// для домашнего задания номер 3
+let books = [
+    { author: 'Толстой', title: 'Война и мир'},
+    { author: 'Гончаров', title: 'Обломов'},
+    { author: 'Лермонтов', title: 'Герой Нашего Времени'}
+];
+
+ */
