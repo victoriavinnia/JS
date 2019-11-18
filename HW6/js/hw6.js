c89b82510635084056fbfc5b8beaaada4296f9df
victoriavinnia <victoriavinnia@gmail.com>
2019-11-19 00:39:37 +0300

    hw6,hw7

---
+++ HW6/js/hw6.js
@@ -0,0 +1,94 @@
+let articles = [
+    {
+        id: 1,
+        title: "JS",
+        text: "Статья про JS",
+        author: "Александр"
+    },
+    {
+        id: 2,
+        title: "PHP",
+        text: "Статья про PHP",
+        author: "Виталий"
+    },
+    {
+        id: 3,
+        title: "Базы Данных",
+        text: "Статья про Базы Данных",
+        author: "Евгения"
+    },
+    {
+        id: 4,
+        title: "HTML",
+        text: "Статья про HTML",
+        author: "Виталий"
+    }
+];
+
+let goods = [
+    {
+        title: "Пианино",
+        price: 3000,
+        count: 25
+    },
+    {
+        title: "Гитара",
+        price: 1200,
+        count: 40
+    },
+    {
+        title: "Барабаны",
+        price: 2700,
+        count: 12
+    },
+    {
+        title: "Флейта",
+        price: 900,
+        count: 50
+    },
+    {
+        title: "Арфа",
+        price: 3400,
+        count: 5
+    }
+];
+
+// примеры вызова функции:
+//generateTable(articles); // генерация таблицы со статьями
+//generateTable(goods); // генерация таблицы с товарами
+
+function generateTable (array) {
+    let newTable = document.createElement("table");
+    let caption = newTable.createCaption();
+    caption.innerText = "Заголовок таблицы";
+
+    for (let y = 0; y < array.length; y++) {
+        let arrayNames = Object.getOwnPropertyNames(array[y]);
+        let arrayValues = Object.values(array[y]);
+    //    console.log(arrayNames);
+    //    console.log(arrayValues);
+        let newRow = newTable.insertRow(y);
+        console.log(newRow);
+
+        for(let x = 0; x < arrayValues.length; x++) {
+            if (y === 0) {
+                let cell = newRow.insertCell(x);
+                cell.innerText = arrayNames[x].toUpperCase();
+                console.log(cell);
+            }else {
+                let cell = newRow.insertCell(x);
+                cell.innerText = arrayValues[x];
+                console.log(cell);
+            }
+
+        }
+
+    }
+
+  document.body.append(newTable);
+
+
+}
+generateTable(goods);
+generateTable(articles); // не выводит 1 объект
+
---
+++ HW7/js/hw7.js
@@ -0,0 +1,85 @@
+// 3.Дан чекбокс и поле для ввода текста, если чекбокс отмечен галочкой,
+// то поле должно быть доступным для ввода данных,
+// в противном случае, поле остается заблокированным.
+let form = document.forms.lesson;
+console.log(form);
+let checkbox = form.elements.checkbox;
+console.log(checkbox);
+let text = form.elements.text;
+console.log(text);
+
+checkbox.addEventListener("click", clickOnElem);
+
+function clickOnElem() {
+    if (checkbox.checked) {
+        text.disabled = false;
+
+    } else {
+        text.disabled = true;
+    }
+}
+// 4. Дана форма и два поля: логин и пароль. Длина логина от 4 до 10 символов, пароля от 6.
+// Реализовать обработку следующих событий:
+// 4.1 Фокус на элементе - подсказка рядом с input красного цвета.
+let login = form.elements.login;
+let pwd = form.elements.pwd;
+
+login.addEventListener("focus", focusOnElem);
+pwd.addEventListener("focus", focusOnElem);
+
+function focusOnElem() {
+    console.log("фокус на элементе");
+    this.nextElementSibling.classList.add("error");
+}
+
+// 4.2 Фокус ушел с элемента: если данные введены верно посказка становится зеленой,
+// в противном случае остается красной.
+login.addEventListener("blur", focusOutLogin);
+pwd.addEventListener("blur", focusOutPwd);
+
+function focusOutLogin() {
+    if(this.value.length < 4 || this.value.length > 10) {
+        this.nextElementSibling.classList.add("error");
+    } else {
+        this.nextElementSibling.classList.remove("error");
+        this.nextElementSibling.classList.add("success");
+    }
+}
+function focusOutPwd() {
+    if(this.value.length < 6) {
+        this.nextElementSibling.classList.add("error");
+    } else {
+        this.nextElementSibling.classList.remove("error");
+        this.nextElementSibling.classList.add("success");
+    }
+}
+
+//4.3 Отправка формы - вывести значение каждого input в консоль
+
+form.addEventListener("submit", getForm);
+function getForm(event) {
+    event.preventDefault();
+    console.log("отправка формы");
+ //   console.log(this.elements.login.value);
+//    console.log(this.elements.pwd.value);
+ //   console.log(this.elements.color.value);
+    let formData = new FormData(this);
+    console.log(formData.getAll("lang[]"));
+    console.log(formData.get("login"));
+    console.log(formData.get("pwd"));
+    console.log(formData.get("color"));
+    console.log(formData.get("text"));
+}
+
+//4.4 Очистка полей формы (reset) - все введенные пользователем данные стираются, все подсказки становятся красного цвета.
+
+
+
+
+
+
+
+
+
+
+
